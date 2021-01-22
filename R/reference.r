03d6df525b66bd818dd3baff90ee164221feb42c
Gibran Hemani <explodecomputer@gmail.com>
2021-01-22 21:58:52 +0000

    added first version of bv ldsc
    tests show that use of just 20k SNPs leads to unusable estimates.
    could try to improve snplist to be ~1m - but will be really too large to be obtained from remote server

--- R/reference.r
+++ R/reference.r
@@ -12,6 +12,20 @@
 
 
 
+#' Retrieve a list of ~20k variants that are common in all five human super populations
+#'
+#' Data frame includes 1000 genomes metadata including sample sizes, allele frequency and LD score
+#'
+#' @export
+#' @return Data frame
+common_snpinfo <- function()
+{
+	select_api("dev1")
+	api_query("variants/afl2/snplist") %>% 
+		get_query_content() %>%
+		dplyr::as_tibble()
+}
+
 
 
 
@@ -22,13 +36,14 @@
 #' @param d Data frame containing at least rsid and eaf columns. e.g. output from associations
 #'
 #' @export
-#' @return vector ordered by most likely ancestry
-infer_ancestry <- function(d)
+#' @return data frame ordered by most likely ancestry
+infer_ancestry <- function(d, snpinfo=NULL)
 {
-	select_api("dev1")
-	snpinfo <- api_query("variants/afl2/snplist") %>% 
-		get_query_content() %>%
-		dplyr::as_tibble() %>%
+	if(is.null(snpinfo))
+	{
+		snpinfo <- common_snpinfo()
+	}
+	snpinfo <- snpinfo %>%
 		dplyr::inner_join(., d, by="rsid")
 
 	nom <- grep("^AF\\.", names(snpinfo), value=TRUE)
@@ -36,47 +51,118 @@ infer_ancestry <- function(d)
 	{
 		cor(snpinfo$eaf, snpinfo[[x]], use="pair")
 	}) %>% sort(decreasing=TRUE)
-	names(out) <- gsub("AF\\.", "", names(out))
+	out <- dplyr::tibble(pop=gsub("AF\\.", "", names(out)), cor=out)
 	return(out)
 }
 
-bv_ldsc <- function(id1, id2, ancestry)
+
+#' Perform basic version of bivariate LD score regression
+#'
+#' For best results please use the LDSC software. This version is a rough approximation that uses only 20k SNPs for the purposes of minimising server load. 
+#'
+#' @param id1 ID of trait 1
+#' @param id2 ID of trait 2
+#' @param ancestry ancestry of traits 1 and 2 (AFR, AMR, EAS, EUR, SAS) or 'infer' (default) in which case it will try to guess based on allele frequencies
+#'
+#' @export
+#' @return List of results
+bv_ldsc <- function(id1, id2, ancestry="infer", snpinfo=NULL)
 {
-	snpinfo <- api_query("variants/afl2/snplist") %>% 
-		get_query_content() %>%
-		dplyr::as_tibble() %>%
-		dplyr::select(rsid, l2=paste0("L2.", ancestry))
+	if(is.null(snpinfo))
+	{
+		snpinfo <- common_snpinfo()
+	}
 
 	d1 <- associations(snpinfo$rsid, id1, proxies=FALSE) %>%
-		d1 <- d1 %>% fill_n() %>%
+		fill_n() %>%
 		dplyr::mutate(z = beta / se) %>%
 		dplyr::select(rsid, z1 = z, n1 = n)
 
 	d2 <- associations(snpinfo$rsid, id2, proxies=FALSE) %>%
-		d2 <- d2 %>% fill_n() %>%
+		fill_n() %>%
 		dplyr::mutate(z = beta / se) %>%
 		dplyr::select(rsid, z2 = z, n2 = n)
 
-	mod <- dplyr::inner_join(snpinfo, d1, by="rsid") %>%
+	if(ancestry == "infer")
+	{
+		ancestry1 <- infer_ancestry(d1, snpinfo)
+		ancestry2 <- infer_ancestry(d2, snpinfo)
+		if(ancestry1$pop[1] != ancestry2$pop[1])
+		{
+			stop("d1 ancestry is ", ancestry1$pop[1], " and d2 ancestry is ", ancestry2$pop[1])
+		}
+		ancestry <- ancestry1$pop[1]
+	}
+
+	dat1 <- snpinfo %>% 
+		dplyr::select(rsid, l2=paste0("L2.", ancestry)) %>%
+		dplyr::inner_join(., d1, by="rsid")
+
+	mod1 <- summary(lm(z1 ~ l2, dat1))
+
+	res <- list()
+	res$id1 <- list()
+	res$id1$intercept <- coefficients(mod1)[1,1]
+	res$id1$intercept_se <- coefficients(mod1)[1,2]
+	res$id1$intercept_pval <- coefficients(mod1)[1,4]
+	res$id1$h2 <- coefficients(mod1)[2,1] * nrow(dat1)
+	res$id1$h2_se <- coefficients(mod1)[2,2] * nrow(dat1)
+	res$id1$h2_pval <- coefficients(mod1)[2,4]
+
+	dat2 <- snpinfo %>% 
+		dplyr::select(rsid, l2=paste0("L2.", ancestry)) %>%
+		dplyr::inner_join(., d2, by="rsid")
+
+	mod2 <- summary(lm(z2 ~ l2, dat2))
+
+	res$id2 <- list()
+	res$id2$intercept <- coefficients(mod2)[1,1]
+	res$id2$intercept_se <- coefficients(mod2)[1,2]
+	res$id2$intercept_pval <- coefficients(mod2)[1,4]
+	res$id2$h2 <- coefficients(mod2)[2,1] * nrow(dat2)
+	res$id2$h2_se <- coefficients(mod2)[2,2] * nrow(dat2)
+	res$id2$h2_pval <- coefficients(mod2)[2,4]
+
+
+	dat <- snpinfo %>% 
+		dplyr::select(rsid, l2=paste0("L2.", ancestry)) %>%
+		dplyr::inner_join(., d1, by="rsid") %>%
 		dplyr::inner_join(., d2, by="rsid") %>%
 		dplyr::mutate(
 			zz = z1 * z2, 
 			n1 = as.numeric(n1),
 			n2 = as.numeric(n2),
 			rhs = l2 * sqrt(n1 * n2)
-		) %>%
-		{summary(lm(zz ~ rhs, .))}
+		)
 
-	dat$zz <- dat$z1
+	mod <- summary(lm(zz ~ rhs, dat))
 
+
+	res$gcov = coefficients(mod)[2,1] * nrow(dat)
+	res$gcov_se = coefficients(mod)[2,2] * nrow(dat)
+	res$gcor = res$gcov / sqrt(res$id1$h2 * res$id2$h2)
+	res$intercept = coefficients(mod)[1,1]
+	res$intercept_se = coefficients(mod)[1,2]
+	res$intercept_pval = coefficients(mod)[1,4]
+	res$n1 <- mean(dat$n1, na.rm=T)
+	res$n2 <- mean(dat$n2, na.rm=T)
+	res$intercept_numerator = res$intercept / sqrt(res$n1 * res$n2)
+
+	return(res)
 }
 
 
 
+#' Look up sample sizes when meta data is missing from associations
+#'
+#' @param d Output from \code{associations}
+#'
+#' @export
+#' @return Updated version of d
 fill_n <- function(d)
 {
 	id <- d$id[1]
-	if(! "n" %in% names(d1))
+	if(! "n" %in% names(d))
 	{
 		d$n <- NA
 	}
--- tests/testthat/test_reference.r
+++ tests/testthat/test_reference.r
@@ -1,6 +1,28 @@
 library(ieugwasr)
 context("reference")
 
-test_that(
+snpinfo <- common_snpinfo()
 
-)
+test_that("snplist", {
+
+	expect_true(nrow(snpinfo) > 10000)
+
+})
+
+test_that("ancestry", {
+
+	a <- associations(snpinfo$rsid, "bbj-a-10", prox=FALSE)
+	res <- infer_ancestry(a, snpinfo)
+	expect_true(res$pop[1] == "EAS")
+})
+
+
+test_that("ldsc", {
+
+	id1 <- "ukb-a-248"
+	id2 <- "ukb-b-19953"
+	ancestry <- "EUR"
+	res <- bv_ldsc(id1, id2, ancestry, snpinfo=snpinfo)
+	expect_true(is.list(res))
+})
+

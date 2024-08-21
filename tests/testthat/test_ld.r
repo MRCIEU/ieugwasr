883f3d3980fe8ae75f876b8bf8938015e85d35e7
Tom Palmer <remlapmot@hotmail.com>
2024-08-21 16:21:44 +0100

    Skip tests if server non-response

--- tests/testthat/test_ld.r
+++ tests/testthat/test_ld.r
@@ -4,10 +4,12 @@ skip_on_cran()
 
 library(dplyr)
 
-a <- tophits("ieu-a-2")
-if(inherits(a, "response")) skip("Server issues")
-ap <- tophits("ieu-a-2", force_server=TRUE)
-au <- tophits("ieu-a-2", clump=1)
+a <- try(tophits("ieu-a-2"))
+if (inherits(a, "try-error")) skip("Server issues")
+ap <- try(tophits("ieu-a-2", force_server=TRUE))
+if (inherits(ap, "try-error")) skip("Server issues")
+au <- try(tophits("ieu-a-2", clump=1))
+if (inherits(au, "try-error")) skip("Server issues")
 b <- dplyr::tibble(rsid=au$rsid, pval=au$p, id=au$id, clump=0)
 bc <- ld_clump(b)
 # bcl <- ld_clump(b, bfile="/Users/gh13047/data/ld_files/data_maf0.01_rs", plink_bin="plink")
@@ -31,26 +33,35 @@ test_that("ld matrix", {
 	)
 })
 
-ab <- tophits(c("ieu-a-2", "ieu-a-1001"))
-expect_warning(ab2 <- ld_clump(ab))
 test_that("multiple", {
+  ab <- try(tophits(c("ieu-a-2", "ieu-a-1001")))
+  if (inherits(ab, "try-error")) skip("Server issues")
+  ab2 <- try(ld_clump(ab))
+  if (inherits(ab2, "try-error")) {
+    skip("Server issues")
+  } else {
+    expect_warning(ab2)
+  }
+  
 	expect_equal(
 		length(unique(ab2$id)), length(unique(ab$id))
 	)
 })
 
 
-a <- tophits(c("ieu-a-2", "ieu-a-7")) %>% subset(., !duplicated(id))
-expect_warning(ab <- ld_clump(a))
 test_that("onesnp", {
+  th <- try(tophits(c("ieu-a-2", "ieu-a-7")))
+  if (inherits(th, "try-error")) skip("Server issues")
+  a <- th %>% subset(., !duplicated(id))
+  expect_warning(ab <- ld_clump(a))
 	expect_equal(nrow(ab), 2)
 })
 
 
 
 test_that("ld_reflookup", {
-
-	a <- ld_reflookup(c("rs234", "fakesnp"), pop="AFR")
+	a <- try(ld_reflookup(c("rs234", "fakesnp"), pop="AFR"))
+	if (inherits(a, "try-error")) skip("Server issues")
 	expect_true(a == "rs234")
 })
 

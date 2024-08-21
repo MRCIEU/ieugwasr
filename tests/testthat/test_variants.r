0f1214ea3475cf38ba0880635468882f06f1e6ff
Tom Palmer <remlapmot@hotmail.com>
2024-08-21 16:05:57 +0100

    Wrap API calls in try()

--- tests/testthat/test_variants.r
+++ tests/testthat/test_variants.r
@@ -3,10 +3,11 @@ skip_on_cran()
 # skip_on_ci()
 
 
-o1 <- variants_gene("ENSG00000123374")
-if(inherits(o1, "response")) skip("Server issues")
+o1 <- try(variants_gene("ENSG00000123374"))
+if (inherits(o1, "try-error")) skip("Server issues")
 
-o2 <- variants_gene("ENSG00000123374", 100000)
+o2 <- try(variants_gene("ENSG00000123374", 100000))
+if (inherits(o2, "try-error")) skip("Server issues")
 
 test_that("genes",
 {
@@ -17,21 +18,26 @@ test_that("genes",
 
 test_that("chrpos",
 {
-	o <- variants_chrpos("7:105561135-105563135")
+	o <- try(variants_chrpos("7:105561135-105563135"))
+	if (inherits(o, "try-error")) skip("Server issues")
 	expect_true(nrow(o) > 1)
 
-	o <- variants_chrpos("7:105561135")
+	o <- try(variants_chrpos("7:105561135"))
+	if (inherits(o, "try-error")) skip("Server issues")
 	expect_true(nrow(o) == 1)
 
-	o <- variants_chrpos("nonsense")
+	o <- try(variants_chrpos("nonsense"))
+	if (inherits(o, "try-error")) skip("Server issues")
 	expect_true(class(o) == "response")
 })
 
 
 test_that("rsid", 
 {
-	o <- variants_chrpos("7:105561135-105563135")
-	p <- variants_rsid(o$name)
+	o <- try(variants_chrpos("7:105561135-105563135"))
+	if (inherits(o, "try-error")) skip("Server issues")
+	p <- try(variants_rsid(o$name))
+	if (inherits(p, "try-error")) skip("Server issues")
 	expect_true(all(nrow(o) == nrow(p)))
 	expect_true(all(o$name == p$name))
 })
@@ -39,13 +45,16 @@ test_that("rsid",
 
 test_that("conversion",
 {
-	o <- variants_to_rsid(c("rs1205", "7:105561135"))
+	o <- try(variants_to_rsid(c("rs1205", "7:105561135")))
+	if (inherits(o, "try-error")) skip("Server issues")
 	expect_true(length(o) == 2)
 
-	o <- variants_to_rsid(c("rs234", "7:105561135"))
+	o <- try(variants_to_rsid(c("rs234", "7:105561135")))
+	if (inherits(o, "try-error")) skip("Server issues")
 	expect_true(length(o) == 1)
 
-	o <- variants_to_rsid(c("rs234"))
+	o <- try(variants_to_rsid(c("rs234")))
+	if (inherits(o, "try-error")) skip("Server issues")
 	expect_true(length(o) == 1)
 })
 

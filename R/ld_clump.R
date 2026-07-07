6ff301cb6fd2d64b42a2f57080c221c9f98ae738
Tom Palmer <remlapmot@hotmail.com>
2026-07-07 13:14:26 +0100

    Align ld_clump_api() defaults with its documentation

--- R/ld_clump.R
+++ R/ld_clump.R
@@ -109,7 +109,7 @@ ld_clump <- function(dat=NULL, clump_kb=10000, clump_r2=0.001, clump_p=0.99,
 #' @param opengwas_jwt Used to authenticate protected endpoints. Login to <https://api.opengwas.io> to obtain a jwt. Provide the jwt string here, or store in .Renviron under the keyname OPENGWAS_JWT.
 #' @param ... Additional arguments passed to `api_query()`.
 #' @return Data frame of only independent variants
-ld_clump_api <- function(dat, clump_kb=10000, clump_r2=0.1, clump_p, pop="EUR", opengwas_jwt=get_opengwas_jwt(), ...)
+ld_clump_api <- function(dat, clump_kb=10000, clump_r2=0.001, clump_p=1, pop="EUR", opengwas_jwt=get_opengwas_jwt(), ...)
 {
 	res <- api_query('ld/clump',
 			query = list(
